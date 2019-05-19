8f31ebb994fa258c9b4aa2d1ade7ef33c951c9ea
oscar2706 <spat_2706@outlook.com>
2019-05-19 13:32:17 -0500

    Guarda y actualiza reaccion en MeGustaArticulo

--- updateLike.php
+++ updateLike.php
@@ -7,8 +7,8 @@ $db_contrasena = "";
 $dbname = "proyectoBM";
 
 $idArticulo = $_POST['idArticulo'];
-$idUsuario = $_POST['idUsuario'];
-$gusta = $_POST['gusta'];
+$idUsuario = $_POST['idUsuario']; //Usar la variable de sesion con idUsuario
+$reaccion = $_POST['reaccion'];
 
 try {
 	$conn = new PDO("mysql:host=$db_servername;dbname=$dbname", $db_usuario, $db_contrasena);
@@ -24,3 +24,33 @@ $stmt = $conn->prepare($sql);
 $stmt->execute([$idArticulo]);
 $idTema = $stmt->fetch();
 
+//1=gusta, 2=disgusta y 3=mhe
+$sql = "SELECT reaccion FROM MeGustaArticulo WHERE idUsuario = ? AND idArticulo = ?";
+$stmt = $conn->prepare($sql);
+$stmt->execute([$idUsuario, $idArticulo]);
+
+$gustaRegistrado = false;
+if ($stmt->rowCount() == 1) {
+	$gustaUsuario = $stmt->fetch();
+	$gustaRegistrado = true;
+
+	$sql = "UPDATE MeGustaArticulo SET reaccion = ? WHERE idUsuario = ? AND idArticulo = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->execute([$reaccion, $idUsuario, $idArticulo]);
+	echo "Guardado";
+
+} else {
+	$sql = "INSERT INTO MeGustaArticulo (idUsuario, idArticulo, reaccion) VALUES (?, ?, ?)";
+	$stmt = $conn->prepare($sql);
+	$stmt->execute([$idUsuario, $idArticulo, $reaccion]);
+
+	$sql = "SELECT reaccion FROM MeGustaArticulo WHERE idUsuario = ? AND idArticulo = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->execute([$idUsuario, $idArticulo]);
+	$gustaUsuario = $stmt->fetch();
+	$gustaRegistrado = true;
+	echo "Guardado";
+}
+
+// if ($gustaRegistrado)
+	
